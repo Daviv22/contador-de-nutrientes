96ed4dfff9ad0181d75582a6aa46d1c99b45607e
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-01-20 17:37:25 -0300

    Criado objeto banana para teste

---
+++ contador-v2/script.js
@@ -0,0 +1,11 @@
+const banana = {
+    id: 'banana',
+    nome: 'Banana',
+    categoria: 'Frutas',
+    imagem: './img/banana.jpg',
+    nutrientes: {
+        carb: 22.84,
+        gord: 0.33,
+        prot: 1.09
+    }
+}
