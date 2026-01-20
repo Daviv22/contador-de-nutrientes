db9acdd5b675146cb4aa4be8b428fd8464c3ed1d
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-01-20 19:58:56 -0300

    Adicionado ao grid o restante de possibilidades de categorias de alimentos

--- contador-v2/script.js
+++ contador-v2/script.js
@@ -133,14 +133,17 @@ function criarCardAlimento(alimento) {
 
 }
 
-const grid = {
+const grids = {
     frutas: document.getElementById('grid-frutas'),
+    animais: document.getElementById('grid-animais'),
+    graos: document.getElementById('grid-graos'),
+    tuberculos: document.getElementById('grid-tuberculos'),
 }
 
 function renderizarCards(listaAlimentos) {
     listaAlimentos.forEach(alimento => {
         const categoria = alimento.categoria
-        const container = grid[categoria]
+        const container = grids[categoria]
 
         const card = criarCardAlimento(alimento)
         container.appendChild(card)
