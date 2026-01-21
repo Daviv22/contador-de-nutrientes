8d45582663d56a111856bfd8945b70dd00f6ca42
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-01-20 21:01:53 -0300

    Função criarCardAlimento exportada

--- contador-v2/js/cardBuilder.js
+++ contador-v2/js/cardBuilder.js
@@ -90,7 +90,7 @@ function criarCardAlimentoFooter (alimento) {
     return footer
 }
 
-function criarCardAlimento(alimento) {
+export function criarCardAlimento(alimento) {
 
     const article = document.createElement('article');
     article.classList.add('alimento-card')
