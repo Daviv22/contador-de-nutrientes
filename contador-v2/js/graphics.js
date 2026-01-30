a245db1834d5f61418172c95ea7146797b1d7471
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-01-30 16:05:26 -0300

    Criada função para calcular kcal

--- contador-v2/js/graphics.js
+++ contador-v2/js/graphics.js
@@ -3,6 +3,13 @@ export function pegarPorcao(alimento, quantidade) {
         alimentoId: alimento.id,
         alimentoNome: alimento.nome,
         quantidade: quantidade
-
     })
+}
+
+function calcularKcal(alimento, porcao) {
+    const carb = alimento.nutriente.carboidratos * porcao;
+    const prot = alimento.nutriente.proteinas * porcao;
+    const gord = alimento.nutriente.gorduras * porcao;
+
+    return carb * 4 + prot * 4 + gord * 9;
 }
