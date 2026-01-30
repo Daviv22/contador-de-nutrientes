cf5342af5bd5c0869285d0a00c2269bbb521ea53
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-01-30 15:32:45 -0300

    Criada função pegarPorcao

---
+++ contador-v2/js/graphics.js
@@ -0,0 +1,8 @@
+export function pegarPorcao(alimento, quantidade) {
+    console.log({
+        alimentoId: alimento.id,
+        alimentoNome: alimento.nome,
+        quantidade: quantidade
+
+    })
+}
