0ec6e5e7f77f5f04fb3e8fe3e7cbbfe6cd9d864d
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-02-01 20:46:21 -0300

    Função criarGraficoPizza criada

--- contador-v2/js/graphics.js
+++ contador-v2/js/graphics.js
@@ -32,3 +32,68 @@ export function atualizarGraficoKcal(alimento, porcao) {
     chartKcal.xAxis[0].categories.push(alimento.nome)
     chartKcal.series[0].addPoint(kcal)
 }
+
+export function criarGraficoPizza(alimento, containerId) {
+    const dados = [
+        {
+            name: 'Carboidratos',
+            y: alimento.nutrientes.carboidratos,
+        },
+        {
+            name: 'Proteínas',
+            y: alimento.nutrientes.proteinas,
+        },
+        {
+            name: 'Gorduras',
+            y: alimento.nutrientes.gorduras,
+        }
+    ];
+
+    Highcharts.chart(containerId, {
+        chart: {
+            type: 'pie',
+            height: 200,
+            backgroundColor: 'transparent',
+            margin: [0, 0, 0, 0],
+            spacing: [0, 0, 0, 0]
+        },
+        title: {
+            text: null
+        },
+        tooltip: {
+            pointFormat: '<b>{point.y:.1f}g</b> ({point.percentage:.1f}%)',
+            style: {
+                fontSize: '12px'
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: false,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b><br>{point.y:.1f}g',
+                    style: {
+                        fontSize: '11px',
+                        textOutline: 'none',
+                        fontWeight: 'bold'
+                    },
+                    distance: 10
+                },
+                showInLegend: false,
+                size: '100%',
+                innerSize: '40%', // Donut chart (opcional)
+                borderWidth: 2,
+                borderColor: '#ffffff'
+            }
+        },
+        series: [{
+            name: 'Nutrientes',
+            colorByPoint: true,
+            data: dados
+        }],
+        credits: {
+            enabled: false
+        }
+    });
+}
