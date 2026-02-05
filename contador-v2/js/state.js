6a50b8d1b2918e5179143e9294e3fc1cf92def68
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-02-04 23:15:12 -0300

    Objeto state criado

---
+++ contador-v2/js/state.js
@@ -0,0 +1,14 @@
+const state = {
+    alimentos: [],
+    metas: {
+        carboidratos: 0,
+        proteinas: 0,
+        gorduras: 0,
+    },
+    refeicoes: {
+        1: [],
+        2: [],
+        3: [],
+        4: []
+    }
+};
